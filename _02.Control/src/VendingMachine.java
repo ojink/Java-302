f4d88efce91d5534cea6fe86ef6abca18c8f22cb
ojink <ojink2@naver.com>
2024-05-07 16:54:05 +0900

    자판기

---
+++ _02.Control/src/VendingMachine.java
@@ -0,0 +1,28 @@
+
+public class VendingMachine {
+	public static void main(String[] args) {
+		//자판기메뉴
+		System.out.println("--- 자판기메뉴 ---");
+		System.out.println("1.콜라: 1250원 ");
+		System.out.println("2.레쓰비: 800원 ");
+		System.out.println("3.코코팜: 1000원 ");
+		//각 메뉴의 금액을 담을 변수 선언
+				
+		//돈을 투입구에 넣기
+		System.out.println("투입구에 돈을 넣으세요");
+		//키보드로 입력해서 돈을 투입하기: 투입한 돈을 담을 변수 선언
+		
+		//투입금액에 따라 선택할 수 있는 음료에 선택가능 램프가 켜지도록 하기
+		
+		//메뉴선택
+		System.out.println("음료를 선택하세요");
+		//선택한 메뉴에 따른 거스름돈 계산하기
+				
+		//제품이 나오고, 거스름돈이 나온다
+		//1000원짜리 몇개
+		//500원짜리 몇개
+		//100원짜리 몇개
+		//50원짜리 몇개
+		
+	}
+}
