5d89af705d90936396e28c3a440c6e2ab673574f
ojink <ojink2@naver.com>
2024-05-09 17:05:27 +0900

    구구단 For

---
+++ _02.Control/src/GugudanFor.java
@@ -0,0 +1,16 @@
+public class GugudanFor {
+	public static void main(String[] args) {
+		//구구단 7단 출력하기
+		//7 X 1 = 7
+		//7 X 9 = 63
+		int dan = 7;
+		for(int by=1; by<=9; by++) {
+			System.out.printf("%d X %d = %d \n", dan, by, dan*by);
+		}
+		
+	}
+}
+
+
+
+
