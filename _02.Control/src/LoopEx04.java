18a8f79203f06d2dc76368783b3cd8aa3ee42cfe
ojink <ojink2@naver.com>
2024-05-10 17:09:48 +0900

    별찍기(사각형)

---
+++ _02.Control/src/LoopEx04.java
@@ -0,0 +1,25 @@
+
+public class LoopEx04 {
+	public static void main(String[] args) {
+		//**********
+		//**********
+		//**********
+		//**********
+		//**********
+		
+		//"**********" 를 5번 출력
+		for(int no=1; no<=5; no++) {
+			//System.out.println("**********");
+			//"*" 를 10번 출력
+			for(int col=1; col<=10; col++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		//no    1           2
+		//col  1~10         1~10
+		//     **********
+		//     *
+	}
+}
