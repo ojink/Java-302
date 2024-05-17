00362c1a0e76a088453fbb38187a1a095ed39502
OH <ojink2@naver.com>
2024-05-17 16:48:09 +0900

    주사위게임 메소드

---
+++ _03.Method/src/DiceGame.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+
+public class DiceGame {
+	
+	//주사위 던지기 메소드
+	static int throwDice( int player, String name ) {
+		Random r = new Random();
+		int dice1, dice2;
+		//홍길동
+		do {
+			dice1 = r.nextInt(6) + 1;
+			dice2 = r.nextInt(6) + 1;
+			player += dice1 + dice2;
+			System.out.printf("%s: (%d,%d) -> %d칸 이동 ==> 최종위치: %d \n"
+							, name, dice1, dice2, dice1+dice2, player);
+			if( dice1==dice2 ) {
+				System.out.println(name + " 한번 더!");
+			}
+		}while(dice1==dice2);
+		
+		return player;
+	}
+	
+	public static void main(String[] args) {
+//두 개의 주사위를 던져서 합한 수 만큼 이동하기
+//두 명이 함께 게임을 한다
+//50칸이 있는 말판을 끝까지 이동하면 게임이 끝난다
+//만약 두 주사위가 같은 수가 나오면 추가로 한 번 더 던질수 있다
+		//두 주사위는 1~6까지 랜덤하게 추출
+		Random r = new Random();
+		int no=0;
+		int hong=0, park=0; //각 게임자의 이동 최종위치를 담을 변수
+		while( true ) {
+			
+			System.out.printf("[%d회차] 주사위를 던지세요~ \n", ++no);
+			
+			//홍길동
+			hong = throwDice(hong, "홍길동");
+			
+			//박문수
+			park = throwDice(park, "박문수");
+			
+			
+			//홍길동/박문수의 최종위치가 50이상이면 게임끝, 둘 따 50이상이 되면 무승부
+			if( hong>=50 && park>=50 ) {
+				System.out.println("무승부");
+				break;
+			}else if( hong>=50 || park>=50 ) {
+				System.out.println("게임끝 승자: " 
+									+  (hong>=50 ? "홍길동" : "박문수") );
+				break;
+			}
+		}//While끝
+		
+	}
+}
