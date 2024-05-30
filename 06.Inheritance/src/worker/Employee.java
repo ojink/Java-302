675f758074e246415fd17fd6c9c896b9ad5f7c8a
OH <ojink2@naver.com>
2024-05-30 16:49:06 +0900

    다형성, UpCasting/DownCasting

--- 06.Inheritance/src/worker/Employee.java
+++ 06.Inheritance/src/worker/Employee.java
@@ -2,16 +2,16 @@ package worker;
 
 abstract public class Employee {
 //	사번 이름 급여 근로형태
-//	월급여를 계산한다
-	String employeeNo, name, workType;
-	int pay;
-	Employee(String employeeNo, String name
-				, String workType, int pay){
-		this.employeeNo = employeeNo;
+//	월급여를 계산한다	
+	String  name, workType;
+	int employeeNo, pay;
+	static int seq = 24001;
+	Employee( String name, String workType, int pay){
+		this.employeeNo = seq++;
 		this.name = name;
 		this.workType = workType;
 		this.pay = pay;
 	}
 	
-	abstract void getMonthPay();
+	abstract int getMonthPay();
 }
--- 06.Inheritance/src/worker/RegularEmployee.java
+++ 06.Inheritance/src/worker/RegularEmployee.java
@@ -3,16 +3,16 @@ package worker;
 public class RegularEmployee
 		extends Employee{
 //	사번 이름 급여(연봉)  근로형태 보너스
-	int bonus;
-	RegularEmployee(String employeeNo
-			, String name, String workType
+	int bonus; //500%
+	RegularEmployee(String name, String workType
 			, int pay, int bonus){
-		super(employeeNo, name, workType, pay);
+		super(name, workType, pay);
 		this.bonus = bonus;
 	}
 	
 //	월급여를 계산한다
-	void getMonthPay() {
-		
+//	(연봉)/12 * (1+0.5)
+	int getMonthPay() {
+		return (int)( pay / 12 * (1+bonus*0.001) );
 	}
 }
