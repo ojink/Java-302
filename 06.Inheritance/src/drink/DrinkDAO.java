b3ba7409917910aacbe1ee0e55846c6afcc8120f
OH <ojink2@naver.com>
2024-06-03 16:50:35 +0900

    공동작업 음료관리

---
+++ 06.Inheritance/src/drink/DrinkDAO.java
@@ -0,0 +1,10 @@
+package drink;
+
+public interface DrinkDAO {
+//	등록, 목록조회, 정보조회, 변경, 삭제
+	void insert();  //강철리
+	void list();    //김종덕
+	void info();    //김현준
+	void update();  //신명섭
+	void delete();  //이해진
+}
